852f7f4651f046dcd67a517b098eab718c4e1580
Juampy Raccuia <juanpabloraccuia9@gmail.com>
2023-10-19 21:44:42 -0300

    correccion en el app

--- api-colectivos/app.js
+++ api-colectivos/app.js
@@ -2,21 +2,24 @@ import express from "express";
 import cors from "cors";
 import { asientosRouter } from "./asientos";
 import { ventasRouter } from "./ventas";
+import { pasajeroRouter } from "./pasajero";
+import { vendedorRouter } from "./vendedor";
 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use("./asientos",asientosRouter);
-app.use("ventas",ventasRouter)
+app.use("./asientos", asientosRouter);
+app.use("./ventas", ventasRouter);
+app.use("/pasajeros", pasajeroRouter);
+app.use("/vendedor", vendedorRouter)
 
 app.get("/", (req, res) => {
-    res.send("Hola mundo");
-  });
+  res.send("Hola mundo");
+});
 
 // Pongo en funcionamiento la API en puerto 3000
 
 app.listen(3000, () => {
-    console.log("API en funcionamiento");
-  });
-  
+  console.log("API en funcionamiento");
+});
