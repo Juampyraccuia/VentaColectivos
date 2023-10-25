beb57236e245309c49cb631cbceaf570f90bfbab
Juampy Raccuia <juanpabloraccuia9@gmail.com>
2023-10-25 16:17:47 -0300

    correccion de archivos

--- api-colectivos/app.js
+++ api-colectivos/app.js
@@ -5,21 +5,20 @@ import { ventasRouter } from "./ventas.js";
 import { pasajeroRouter } from "./pasajero.js";
 import { vendedorRouter } from "./vendedor.js";
 
-
 const app = express();
+
 app.use(express.json());
 app.use(cors());
-app.use("./asientos", asientosRouter);
-app.use("./ventas", ventasRouter);
+
+app.use("/asientos", asientosRouter);
+app.use("/ventas", ventasRouter);
 app.use("/pasajeros", pasajeroRouter);
-app.use("/vendedor", vendedorRouter)
+app.use("/vendedor", vendedorRouter);
 
 app.get("/", (req, res) => {
   res.send("Hola mundo");
 });
 
-// Pongo en funcionamiento la API en puerto 3000
-
 app.listen(3000, () => {
-  console.log("API en funcionamiento");
+  console.log("API en funcionamiento en el puerto 3000");
 });
---
+++ api-colectivos/destinos.js
@@ -0,0 +1,28 @@
+import { express } from "express"
+import { db } from "./db.js"
+
+export const destinosRouter = express
+    .get("/", async (req, res) => {
+        const [rows, fields] = await db.execute(
+            "SELECT id, valor, destino FROM destino"
+        );
+        res.send(rows)
+    })
+
+    .get("./:id", async (req, res) => {
+        const id = req.params.id;
+        const [rows, fields] = await db.execute(
+            "SELECT id, valor, destino FROM destino",
+            { id }
+        );
+        if (rows.length > 0) {
+            res.send(rows[0])
+        } else {
+            res.status(404).send({ mensaje: "destino no encontrado" })
+        }
+    })
+
+// .get("/:id/destino",async(req,res)=>{
+//     const id=req.params.id;
+//     const [rows,fields]=await db.execute
+// })
