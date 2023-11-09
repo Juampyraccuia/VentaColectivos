f9b9190657995838870770e34b09cb875227f5f6
Juampy Raccuia <juanpabloraccuia9@gmail.com>
2023-11-09 20:55:35 -0300

    Se agrega react, solucion del problema de validar

--- api-colectivos/pasajero.js
+++ api-colectivos/pasajero.js
@@ -1,19 +1,21 @@
 import express from "express";
 import { db } from "./db.js";
+import { body, param, query, validationResult } from "express-validator";
+
 
 
 export const pasajeroRouter = express.Router();
 
 
 pasajeroRouter.get("/", async (req, res) => {
-  const [rows, fields] = await db.execute("SELECT * FROM ventacolectivos.pasajeros");
+  const [rows, fields] = await db.execute("SELECT * FROM ventacolectivos.pasajero");
   res.send(rows);
 })
 
   .get("/:dni", async (req, res) => {
     const dni = req.params.dni;
     const [rows, fields] = await db.execute(
-      "SELECT  nombres, apellido, dni FROM pasajeros WHERE dni = :dni",
+      "SELECT  nombres, apellido, dni FROM pasajero WHERE dni = :dni",
       { dni }
     );
     if (rows.length > 0) {
@@ -23,26 +25,31 @@ pasajeroRouter.get("/", async (req, res) => {
     }
   })
 
-  .post("/", async (req, res) => {
-    const pasajero = req.body;
-    const nombres = pasajero.nombres || null;
-    const apellido = pasajero.apellido || null;
-    const dni = pasajero.dni || null;
-    const fechaNac = pasajero.fechaNac || null;
+  .post("/",
+    body("pasajero.nombres").isAlpha().isLength({ min: 1, max: 100 }),
+    body("pasajero.apellido").isAlpha().isLength({ min: 1, max: 45 }),
+    body("pasajero.dni").isNumeric().isLength({ min: 1, max: 8 }),
+    body("pasajero.fechaNac").isISO8601(),
+    async (req, res) => {
+      const validacion = validationResult(req);
+      if (!validacion.isEmpty()) {
+        res.status(400).send({ errors: validacion.array() });
+        return;
+      }
 
-    const [rows] = await db.execute(
-      `INSERT INTO ventacolectivos.pasajeros (nombres, apellido, dni, fechaNac)
-    VALUES (?, ?, ?, ?)`,
-      [nombres, apellido, dni, fechaNac]
-    );
-
-    res.status(201).send({ nombres, apellido, id: rows.insertId });
-  })
+      const pasajero = req.body.pasajero; // Accede a los datos de pasajero
+      const [rows] = await db.execute(
+        'INSERT INTO ventacolectivos.pasajero (nombres, apellido, dni, fechaNac) VALUES (?, ?, ?, ?)',
+        [pasajero.nombres, pasajero.apellido, pasajero.dni, pasajero.fechaNac]
+      );
+      res.status(201).send({ nombres: pasajero.nombres, apellido: pasajero.apellido, id: rows.insertId });
+    }
+  )
 
   .delete("/:dni", async (req, res) => {
     const dni = req.params.dni;
     const [result] = await db.execute(
-      "DELETE FROM pasajeros WHERE dni = :dni",
+      "DELETE FROM pasajero WHERE dni = :dni",
       { dni }
     );
     if (result.affectedRows > 0) {
---
+++ web-colectivos/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import axios from "axios"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>HOla mundo</h1>
+    </>
+  )
+}
+
+export default App
